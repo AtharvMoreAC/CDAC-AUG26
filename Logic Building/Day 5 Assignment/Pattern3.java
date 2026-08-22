4d4faf86173fefb3580690e10f76267d363ef82d
AtharvMoreAC <atharv.more.cmaug26@gmail.com>
2026-08-22 20:26:37 +0530

    Add files via upload

---
+++ Logic Building/Day 5 Assignment/Pattern3.java
@@ -0,0 +1,22 @@
+public class Pattern3 {
+    public static void main(String[] args) {
+
+        int n = 5;
+		
+        
+        for (int i = 1; i <= n; i++) {
+
+            for (int j = 1; j <= i; j++) {
+				int num = 2 * j - 1;
+                System.out.print(num);
+               
+
+                if (j < i) {
+                    System.out.print("*");
+                }
+            }
+
+            System.out.println();
+        }
+	}
+}
---
+++ Logic Building/Day 5 Assignment/SumOfOddNum.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class SumOfOddNum {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the Number :");
+
+        int n = sc.nextInt();
+
+        int sum =0;
+
+        for(int i = 1; i <= n; i++) {
+            if(i%2!=0) {
+                sum = sum + i;
+            }
+        }
+
+        System.out.println("The sum of odd numbers from 1 to " + n + " is: " + sum);
+    }
+}
