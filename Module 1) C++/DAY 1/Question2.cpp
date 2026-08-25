6e0937d7e4f9d8ac1615b407e5e37a798cce56c7
AtharvMoreAC <atharv.more.cmaug26@gmail.com>
2026-08-25 22:13:29 +0530

    Create Question2.cpp

---
+++ Module 1) C++/DAY 1/Question2.cpp
@@ -0,0 +1,12 @@
+// Area Calculation
+
+#include <iostream>
+using namespace std;
+int main(){
+int  a , b ;
+cout<<"Enter Sides of Square :";
+cin>>a;
+cin>>b;
+cout<<"Area of Square:"<<a*b;
+return 0;
+}
