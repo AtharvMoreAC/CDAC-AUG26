f1f05b0e62055baa47805e637314b2c4c4438d9a
AtharvMoreAC <atharv.more.cmaug26@gmail.com>
2026-09-19 02:05:44 +0530

    Add files via upload

---
+++ Module 2) OOPJ/Assignment 1/1) Calculator/mainfunction.java
@@ -0,0 +1,13 @@
+public class mainfunction {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("entr first no: ");
+		int num1=consoleinput.getint();
+		System.out.println("enter second no : ");
+		float num2=consoleinput.getfloat();
+		System.out.println("sum is : "+calculator.add(num1, num2));
+		
+	}
+
+}
