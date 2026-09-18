7cec97168111f06d0649827e7ea9ff9aa94d508d
AtharvMoreAC <atharv.more.cmaug26@gmail.com>
2026-09-19 01:59:09 +0530

    Add files via upload

---
+++ Module 2) OOPJ/Exercises/3) Class/Question 3/Rectangle.java
@@ -0,0 +1,27 @@
+public class Rectangle {
+
+    int length;
+    int breadth;
+
+    public Rectangle(int length, int breadth) {
+
+        this.length = length;
+        this.breadth = breadth;
+    }
+
+    public int area() {
+
+        return length * breadth;
+    }
+
+    public static void main(String[] args) {
+    	
+    	Rectangle r1 = new Rectangle(4,5);
+    	Rectangle r2 = new Rectangle(5,8);
+    	
+    	System.out.println("Area of first rectangle = " + r1.area());
+    	System.out.println("Area of second rectangle = " + r2.area());
+
+    }
+
+}
