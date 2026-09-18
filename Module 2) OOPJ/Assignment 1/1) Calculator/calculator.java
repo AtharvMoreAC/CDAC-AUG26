dc867c8cd926de5e13e815a5e2a9211b353f6f7e
AtharvMoreAC <atharv.more.cmaug26@gmail.com>
2026-09-19 02:04:38 +0530

    Add files via upload

---
+++ Module 2) OOPJ/Assignment 1/1) Calculator/calculator.java
@@ -0,0 +1,13 @@
+public class calculator {
+	
+	public static int add(int num1 ,int num2) {
+		return num1+num2;
+		
+	}
+	public static float add(float num1,float num2) {
+		return num1+num2;
+	}
+
+	
+
+}
