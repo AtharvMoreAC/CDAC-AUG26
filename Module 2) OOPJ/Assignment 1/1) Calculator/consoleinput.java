556fa03b83398aa86a4d30bdef84404d0a6a38b7
AtharvMoreAC <atharv.more.cmaug26@gmail.com>
2026-09-19 02:06:14 +0530

    Add files via upload

---
+++ Module 2) OOPJ/Assignment 1/1) Calculator/consoleinput.java
@@ -0,0 +1,42 @@
+public class consoleinput {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	public static String getstring() {
+		try {
+			byte arrinput[]=new byte[100];
+			
+			int length = System.in.read(arrinput);
+			byte arrfinal[]=new byte[length-2];
+			System.arraycopy(arrinput, 0, arrfinal, 0, length-2);
+			String objstring= new String(arrfinal);
+			
+			return objstring;
+			
+
+		}catch (Exception e) {
+			e.printStackTrace();
+			
+		}
+	return null;
+	}
+	
+	public static int getint() {
+		String objstring=getstring();
+		int num1=Integer.parseInt(objstring);
+		return num1;
+	}
+	
+	public static float getfloat() {
+		
+//		String objstring=s();
+//	    float num1=Float.parseFloat(objstring);
+//	    return num1;
+		return Float.parseFloat(getstring());
+	    
+	}
+
+
+}
